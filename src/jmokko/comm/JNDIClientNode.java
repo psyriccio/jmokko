f29748e8329fcdfc1e194bc3941e93a9912e9478
psyriccio <psyriccio@psyzzy.net>
2015-06-08 16:07:18 +0300

    Support for JNDI-transport

---
+++ src/jmokko/comm/JNDIClientNode.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmokko.comm;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+import javax.naming.NamingException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.remoting.rmi.JndiRmiProxyFactoryBean;
+
+/**
+ *
+ * @author psyriccio
+ */
+public class JNDIClientNode extends Node {
+
+    private static final Logger log = LogManager.getLogger(JNDIClientNode.class);
+    
+    private final ConcurrentLinkedQueue<TransportMessage> inQueue;
+    private final ConcurrentLinkedQueue<TransportMessage> outQueue;
+    private final Thread transportThread;
+    
+    public JNDIClientNode(NodeDescriptor nodeDescriptor, InetAddress inetAddress, int port, byte[] initData) {
+        super(nodeDescriptor, inetAddress, port);
+        
+        this.inQueue = new ConcurrentLinkedQueue<>();
+        this.outQueue = new ConcurrentLinkedQueue<>();
+        
+        log.info("Staring transport thread");
+        transportThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                log.info("Creating JNDI-factory");
+                JndiRmiProxyFactoryBean jndiProxyFactory = new JndiRmiProxyFactoryBean();
+                jndiProxyFactory.setLookupStubOnStartup(false);
+                jndiProxyFactory.setRefreshStubOnConnectFailure(true);
+                String url = "jndi://" + inetAddress.getHostName() + ":" + Integer.toString(port) + "/jmokko.comm.ITransportPipe";
+                log.info("url> " + url);
+                //jndiProxyFactory.setServiceUrl(url);
+                jndiProxyFactory.setJndiName(url);
+                jndiProxyFactory.setServiceInterface(jmokko.comm.ITransportPipe.class);
+                try {
+                    jndiProxyFactory.afterPropertiesSet();
+                } catch (NamingException ex) {
+                    java.util.logging.Logger.getLogger(JNDIClientNode.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                log.info("JNDI-factory created");
+                log.info("Connecting to ITransportPipe object");
+                ITransportPipe client = (ITransportPipe) jndiProxyFactory.getObject();
+                String sessionId;
+                try {
+                    log.info("call to ITransportPipe.init()");
+                    sessionId = client.init(nodeDescriptor.getUid().toString(), initData);
+                    log.info("returned sessionId = " + sessionId);
+                } catch (Exception ex) {
+                    log.info("Exception in transport thread");
+                    log.catching(ex);
+                    throw new RuntimeException(ex.getMessage());
+                }
+                while(true) {
+                    try {
+                        if(client.messageAvaible(sessionId)) {
+                            inQueue.add(client.get(sessionId));
+                        }
+                        TransportMessage msg = outQueue.poll();
+                        if(msg != null) {
+                            msg.setSessionId(sessionId);
+                            client.put(sessionId, msg);
+                        }
+                        Thread.sleep(300);
+                    } catch (InterruptedException ex) {
+                        log.info("Transport thread interrupted");
+                        log.catching(ex);
+                        break;
+                    } catch (IOException ex) {
+                        log.catching(ex);
+                    }
+                }
+            }
+        });
+        
+        log.info("Transport thread started");
+        
+    }
+
+    public TransportMessage poll() {
+        return inQueue.poll();
+    } 
+    
+    public void post(TransportMessage msg) {
+        outQueue.add(msg);
+    }
+    
+    public void start() {
+        transportThread.start();
+    }
+    
+}
---
+++ src/jmokko/comm/JNDIServerNode.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmokko.comm;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.rmi.NoSuchObjectException;
+import java.rmi.RemoteException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+import javax.naming.NamingException;
+import jmokko.utils.Utils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.remoting.rmi.JndiRmiServiceExporter;
+
+/**
+ *
+ * @author psyriccio
+ */
+public class JNDIServerNode extends Node {
+
+    private static final Logger log = LogManager.getLogger(JNDIServerNode.class);
+    
+    private JndiRmiServiceExporter jndiServiceExporter = null;
+    private final ConcurrentHashMap<String, ConcurrentLinkedQueue<TransportMessage>> queues;
+    private final ConcurrentLinkedQueue<TransportMessage> inQueue;
+    private final SessionManager sessionManager = new SessionManager(10000);
+    private final IAuthenticationService authService;
+    private final ITransportPipe pipe = new ITransportPipe() {
+
+        @Override
+        public String init(String id, byte [] initData) throws IOException {
+            log.info("ITransportPipe.init() " + id + ", initData = " + Utils.bytesToHex(initData));
+            String sessionId = authService.authentication(id, initData);
+            ConcurrentLinkedQueue<TransportMessage> newQueue = new ConcurrentLinkedQueue<>();
+            queues.put(sessionId, newQueue);
+            log.info("Created session id = " + sessionId);
+            return sessionId;
+        }
+        
+        @Override
+        public void close(String sessionId) throws IOException {
+            log.info("ITransportPipe.close()");
+            if(queues.contains(sessionId)) {
+                queues.remove(sessionId);
+            }
+            log.info("Closed session id = " + sessionId);
+        }
+
+        @Override
+        public boolean messageAvaible(String sessionId) throws IOException {
+            return !queues.get(sessionId).isEmpty();
+        }
+
+        @Override
+        public TransportMessage get(String sessionId) throws IOException {
+            return queues.get(sessionId).poll();
+        }
+
+        @Override
+        public void put(String sessionId, TransportMessage msg) throws IOException {
+            inQueue.add(msg);
+        }
+
+    };
+            
+    public JNDIServerNode(NodeDescriptor nodeDescriptor, InetAddress inetAddress, int port, IAuthenticationService authService) {
+        super(nodeDescriptor, inetAddress, port);
+        this.queues = new ConcurrentHashMap<>();
+        this.inQueue = new ConcurrentLinkedQueue<>();
+        this.authService = authService;
+    }
+    
+    public void start() throws RemoteException {
+        log.info("Starting JNDIServerNode");
+        if(jndiServiceExporter == null) {
+            jndiServiceExporter = new JndiRmiServiceExporter();
+            jndiServiceExporter.setJndiName("jmokko.comm.ITransportPipe");
+            jndiServiceExporter.setService(pipe);
+            jndiServiceExporter.setServiceInterface(ITransportPipe.class);
+            try {
+                //jndiServiceExporter.setRegistryHost(getInetAddress().getHostAddress());
+                //jndiServiceExporter.setServicePort(getPort());
+                //jndiServiceExporter.setRegistryPort(getPort());
+                jndiServiceExporter.afterPropertiesSet();
+            } catch (NamingException ex) {
+                java.util.logging.Logger.getLogger(JNDIServerNode.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        log.info("JNDIServerNode started");
+    }
+    
+    public void stop() throws RemoteException {
+        log.info("Stopping JNDIServerNode");
+        if(jndiServiceExporter != null) {
+            try {
+                jndiServiceExporter.destroy();
+            } catch (NamingException ex) {
+                java.util.logging.Logger.getLogger(JNDIServerNode.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NoSuchObjectException ex) {
+                java.util.logging.Logger.getLogger(JNDIServerNode.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            jndiServiceExporter = null;
+        }
+        log.info("JNDIServerNode stopped");
+    }
+
+    public ConcurrentLinkedQueue<TransportMessage> getInQueue() {
+        return inQueue;
+    }
+
+    public ConcurrentHashMap<String, ConcurrentLinkedQueue<TransportMessage>> getQueues() {
+        return queues;
+    }
+    
+}
