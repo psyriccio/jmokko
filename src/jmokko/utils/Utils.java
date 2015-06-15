7d093bd3a2fc850b22b16c50b0baf8741cca59f6
psyriccio <psyriccio@psyzzy.net>
2015-06-15 11:08:09 +0300

    "null" string return from Utils.bytesToHex() if array is null

--- src/jmokko/utils/Utils.java
+++ src/jmokko/utils/Utils.java
@@ -21,6 +21,9 @@ public class Utils {
 
     final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
     public static String bytesToHex(byte[] bytes) {
+        if(bytes == null) {
+            return "null";
+        }
         char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++ ) {
             int v = bytes[j] & 0xFF;
