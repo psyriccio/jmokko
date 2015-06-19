b936e744206ba0b76426f7d48122f101bd3d2e1f
psyriccio <psyriccio@psyzzy.net>
2015-06-19 15:40:51 +0300

    .

--- src/jmokko/crypt/CryptedContainer.java
+++ src/jmokko/crypt/CryptedContainer.java
@@ -11,8 +11,6 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
