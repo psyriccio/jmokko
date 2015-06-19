55b7a90a4ef89ef25fa192dc0e71fd434a52b0bf
psyriccio <psyriccio@psyzzy.net>
2015-06-19 15:32:15 +0300

    pack/unpack content-body

--- src/jmokko/crypt/CryptedContainer.java
+++ src/jmokko/crypt/CryptedContainer.java
@@ -6,10 +6,16 @@
 package jmokko.crypt;
 
 import java.nio.ByteBuffer;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -21,8 +27,8 @@ import javax.crypto.spec.SecretKeySpec;
 public class CryptedContainer {
 
     private static final byte[] MAGIC = { '~', 'C', 'C', 0, '~' };
-    private final byte[] content;
-    private final byte[] body;
+    private byte[] content;
+    private byte[] body;
     private final KeyPairContainer key;
     private SecretKey symKey;
     private byte[] symKeyIV;
@@ -61,13 +67,36 @@ public class CryptedContainer {
         symKey = new SecretKeySpec(keyEnc, "AES");
     }
     
-    public void pack() {
+    public void pack() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
         ByteBuffer bb = ByteBuffer.allocate(1024 * 1024 * 100);
         bb.put(CryptedContainer.MAGIC);
+        CryptHelper crypt = new CryptHelper(CryptAlgoritm.RSA, CryptAlgoritmMode.ECB, CryptPadding.PKCS5Padding, SignatureAlgoritm.SHA512withRSA, key);
+        byte[] cryptedSymKey = crypt.encrypt(getPackedSymKey(), key);
+        bb.put(cryptedSymKey);
+        crypt = new CryptHelper(CryptAlgoritm.AES, CryptAlgoritmMode.CBC, CryptPadding.PKCS1Padding, SignatureAlgoritm.SHA512withRSA, new KeyPairContainer("AES", symKey.getEncoded()), symKeyIV);
+        body = crypt.encrypt(content);
+        bb.putInt(body.length);
+        bb.put(body);
     }
     
-    public void unpack() {
-        
+    public void unpack() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        ByteBuffer bb = ByteBuffer.wrap(body);
+        byte[] magic = new byte[5];
+        byte[] cryptedKey = new byte[64];
+        bb.get(magic);
+        bb.get(cryptedKey);
+        int dataLength = bb.getInt();
+        body = new byte[dataLength];
+        bb.get(body);
+        CryptHelper crypt = new CryptHelper(CryptAlgoritm.RSA, CryptAlgoritmMode.ECB, CryptPadding.PKCS5Padding, SignatureAlgoritm.SHA512withRSA, key);
+        byte[] decryptedKey = crypt.decrypt(cryptedKey);
+        ByteBuffer bbkey = ByteBuffer.wrap(decryptedKey);
+        byte[] encodedKey = new byte[32];
+        bbkey.get(encodedKey);
+        bbkey.get(symKeyIV);
+        symKey = new SecretKeySpec(encodedKey, "AES");
+        crypt = new CryptHelper(CryptAlgoritm.AES, CryptAlgoritmMode.CBC, CryptPadding.PKCS1Padding, SignatureAlgoritm.SHA512withRSA, new KeyPairContainer("AES", encodedKey), symKeyIV);
+        content = crypt.decrypt(body);
     }
     
 }
